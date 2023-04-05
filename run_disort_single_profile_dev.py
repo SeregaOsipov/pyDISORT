Accuracy (negative fluxes) with pyDISORT were fixed by switching to double precision via autodoubling. The RF of Tonga H2O is complete.
Some fixes for the coarse dust RF were implemented including the phase function calculation and normalization.

run_disort_single_profile_dev.py
@@ -6,102 +6,22 @@ import xarray
 import xarray as xr
 import datetime as dt
 
-from lblrtm_utils import write_settings_to_tape, Gas, run_lblrtm, read_od_output, LblrtmSetup, run_lblrtm_over_spectral_range
-from disort_utils import run_disort, DisortSetup, run_disort_spectral, setup_viewing_geomtry, \
-    prep_chanceetal_sun_spectral_irradiance, setup_surface_albedo
+from climpy.utils.atmos_utils import get_atmospheric_profile
+from climpy.utils.lblrtm_utils import Gas, run_lblrtm_over_spectral_range, LblrtmSetup
+from climpy.utils.disort_utils import DisortSetup, run_disort_spectral, setup_viewing_geomtry, \
+    setup_surface_albedo
 
 #%% experimental setup
 lats = np.array([-20.55,])  # location & date to process
 lons = np.array([175.385,])
 date = dt.datetime(1991, 1, 2)
 #%% setup atmospheric meteorological profile
-
-
-def get_atmospheric_profile():
-    fp = '{}sfc/{}'.format('/work/mm0062/b302074/Data/ECMWF/EraInterim/netcdf/global/F128/', 'ECMWF_sfc_19910102_19910102.nc')
-    fp = os.path.expanduser('~') + '/Temp/ECMWF_sfc_19910102_19910102.nc'  # temp local
-    sfc_ds = xr.open_dataset(fp)
-    sfc_ds = sfc_ds.rename_vars({'z': 'z_sfc'})
-    fp = '{}pl/{}'.format('/work/mm0062/b302074/Data/ECMWF/EraInterim/netcdf/global/F128/', 'ECMWF_pl_19910102_19910102.nc')
-    fp = os.path.expanduser('~') + '/Temp/ECMWF_pl_19910102_19910102.nc'  # temp local
-    profile_ds = xr.open_dataset(fp)
-
-    # add pressure variable
-    profile_ds['p'] = (('level', ), profile_ds.level.data)  # keep in 1D although general approach is N-D
-
-    # reverse the z direction to have indexing start at the surface
-    profile_ds = profile_ds.sel(level=slice(None, None, -1))
-
-    # merge surface and profile datasets
-    ds = xr.merge([sfc_ds, profile_ds])
-    ds = ds.rename({'latitude': 'lat', 'longitude': 'lon'})
-
-    # correct few things
-    ds.sp[:] /= 10**2
-    ds.sp.attrs['units'] = 'hPa'
-
-    # convert geopotential to height in meters
-    # TODO: it is probably faster to do it after location sampling or in output parser
-    g = 9.8  # m/sec**2
-    ds.z_sfc[:] /= g
-    ds.z_sfc.attrs['units']='m'
-    ds.z_sfc.attrs['long_name'] = 'Height'
-    ds.z_sfc.attrs['standard_name'] = 'height'
-
-    ds.z[:] /= g
-    ds.z.attrs['units'] = 'm'
-    ds.z.attrs['long_name'] = 'Height'
-    ds.z.attrs['standard_name'] = 'height'
-
-    return ds
-
-
-atm_stag_ds = get_atmospheric_profile()  # stag indicates staggered profile
+atm_stag_ds = get_atmospheric_profile(date)  # stag indicates staggered profile
 atm_stag_ds = atm_stag_ds.sel(lat=lats, lon=lons, time=date, method='nearest')  # TODO: date selection should be controlled by tollerance
 
 # atm_stag_ds.z[0, 0, 0] = 0  # TODO: temp test is z has to start from zero
 atm_stag_ds = atm_stag_ds.isel(level=range(len(atm_stag_ds.level) - 2))
 #%% setup atmospheric chemical composition (gases)
-
-
-def get_atmospheric_gases_composition():
-    fp = '/work/mm0062/b302074/Data/NASA/GMI/gmiClimatology.nc'  # GMI climatology
-    fp = os.path.expanduser('~') + '/Temp/gmiClimatology.nc'  # GMI climatology
-    ds = xr.open_dataset(fp)
-    # fix the metadata
-    ds = ds.set_coords({'lat', 'lon', 'level'})
-    ds = ds.swap_dims({'latitude_dim': 'lat', 'longitude_dim': 'lon', 'eta_dim': 'level'})
-    ds = ds.rename({'species_dim': 'species'})
-
-    species = []  # derive labels from netcdf
-    for index in range(ds.const_labels.shape[1]):
-        label = ''.join([r.item().decode().strip() for r in ds.const_labels[:,index]])
-        species += [label, ]
-
-    ds['species'] = (('species', ), species)
-
-    # SO2 is missing, setup dummy profile.
-    so2_ds = ds.sel(species=Gas.O2.value)  # Use O2 to make a single gas copy as a template for SO2
-    so2_ds.const[:]=0
-    so2_ds['species'] = (('species',), ['SO2',])
-
-    # CO2 is missing, setup 388.5 (as of june 2012).
-    co2_ds = ds.sel(species=Gas.O2.value)  # Use O2 to make a single gas copy as a template for SO2
-    co2_ds.const[:] = 388.5  # units A
-    co2_ds['species'] = (('species',), ['CO2', ])
-
-    ds = xr.concat([ds, so2_ds, co2_ds], 'species')
-    ds['time'] = np.arange(12)  # zero based indexing
-
-    ds = ds.drop(labels=('const_labels',))
-
-    # add units variable according to LBLRTM
-    ds['units'] = (('species',), ['A',]*ds.species.size)
-    ds.units.attrs['long_name'] = 'units according to LBLRTM'
-
-    return ds
-
-
 gases_ds = get_atmospheric_gases_composition()
 gases_ds = gases_ds.sel(lat=lats, lon=lons, time=date.month-1, method='nearest')  # do the selection
 # interpolate gases on the LBLRTM vertical grid
@@ -116,6 +36,10 @@ h2o_ds['units'] = (('species',), ['H',])  # relative humidity
 gases_ds = xr.concat([gases_ds, h2o_ds], 'species')
 
 #%% derive optical properties
+dvout = 10
+lblrtm_setup_vo = LblrtmSetup()
+lblrtm_setup_vo.DVOUT = dvout  # cm^-1
+lblrtm_setup_vo.zenithAngle = 180  # 0
 
 if os.path.exists('ds_with_rayleigh.nc'):
     print('Reusing local copy of the previos LBLRTM calculations')
@@ -135,9 +59,9 @@ else:
 
     # calculate optical properties. Remember that LBLRTM represent rho layer and optical properties of these layers
     print('\nPreparing LBLRTM run with Rayleigh scattering\n')
-    ds = run_lblrtm_over_spectral_range(wn_range, lblrtm_scratch_fp, atm_stag_ds, gases_ds, cross_sections, True)
+    ds = run_lblrtm_over_spectral_range(wn_range, lblrtm_scratch_fp, atm_stag_ds, gases_ds, cross_sections, lblrtm_setup_vo, True)
     print('\nPreparing LBLRTM run without Rayleigh scattering\n')
-    ds_without_Rayleigh = run_lblrtm_over_spectral_range(wn_range, lblrtm_scratch_fp, atm_stag_ds, gases_ds, cross_sections, False)
+    ds_without_Rayleigh = run_lblrtm_over_spectral_range(wn_range, lblrtm_scratch_fp, atm_stag_ds, gases_ds, cross_sections, lblrtm_setup_vo, False)
     ds.to_netcdf('ds_with_rayleigh.nc')
     ds_without_Rayleigh.to_netcdf('ds_without_rayleigh.nc')
 